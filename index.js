a443405a34ba6145dcbf65b93b6d276f3b208cdb
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-08-21 12:10:00 -0500

    feat: desktop version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function shareContent() {
+function shareContentMobile() {
   document.getElementById('share-container').style = 'background-color: hsl(217, 19%, 35%); padding: 20px 32px; justify-content: space-between'
   document.getElementById('container').style.padding = '40px 0 0 0';
   document.getElementById('avatar').style.display = 'none';
@@ -8,14 +8,25 @@ function shareContent() {
   document.querySelector('#icon-container').style.backgroundColor = '#6E8098';
 }
 
+function shareContentDesktop() {
+  document.getElementById('social-icons').style = 'display: flex';
+  document.querySelector('#icon-container svg path').style.fill = 'white';
+  document.querySelector('#icon-container').style.backgroundColor = '#6E8098';
+}
+
 // * Media Query
-const mediaMobile = window.matchMedia('(max-width: 1023px)');
+const mediaMobile = window.matchMedia('(max-width: 767px)');
 
 function handleChange(mediaQuery) {
+  const iconsContainer = document.getElementById('icon-container');
+  const socialContainer = document.getElementById('social-icons');
   if (mediaQuery.matches) {
-    document.getElementById('icon-container').addEventListener('click', shareContent);
+    document.getElementById('user-date').after(socialContainer);
+    iconsContainer.addEventListener('click', shareContentMobile);
   } else {
-    console.log('dont matches');
+    iconsContainer.removeEventListener('click', shareContentMobile);
+    iconsContainer.addEventListener('click', shareContentDesktop);
+    iconsContainer.appendChild(document.getElementById('social-icons'));
   }
 }
 
