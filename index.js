e7f2f08d46bbb1c17d84643ef3e4bcf3531391df
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-08-22 10:52:16 -0500

    Ready for deploy

--- index.js
+++ index.js
@@ -3,30 +3,19 @@ function shareContentMobile() {
   document.getElementById('container').style.padding = '40px 0 0 0';
   document.getElementById('avatar').style.display = 'none';
   document.getElementById('user-date').style.display = 'none';
-  document.getElementById('social-icons').style.display = 'flex'
+  document.getElementById('social-container').style.display = 'flex'
   document.querySelector('#share-button svg path').style.fill = 'white';
   document.querySelector('#share-button').style.backgroundColor = '#6E8098';
 }
 
-// For animations
-let interval;
-
 function shareContentDesktop() {
-  document.getElementById('social-icons').style = 'display: flex';
-  clearInterval(interval);
-  interval = setInterval(() => {
-    document.getElementById('social-icons').style = 'display: flex; opacity: 100%';
-  }, 5);
+  document.getElementById('social-container').style = 'display: flex; opacity: 1';
   document.querySelector('#share-button svg path').style.fill = 'white';
   document.querySelector('#share-button').style.backgroundColor = '#6E8098';
 }
 
 function closeShareBox() {
-  clearInterval(interval);
-  interval = setInterval(() => {
-    document.getElementById('social-icons').style = 'display: flex; opacity: 0';
-  }, 5)
-  document.getElementById('social-icons').removeAttribute('style');
+  document.getElementById('social-container').removeAttribute('style');
   document.querySelector('#share-button svg path').removeAttribute('style');
   document.querySelector('#share-button').removeAttribute('style');
 }
@@ -35,16 +24,16 @@ function closeShareBox() {
 const mediaMobile = window.matchMedia('(max-width: 767px)');
 
 function handleChange(isMobile) {
-  const iconsContainer = document.getElementById('share-button');
-  const socialContainer = document.getElementById('social-icons');
+  const shareButton = document.getElementById('share-button');
+  const socialContainer = document.getElementById('social-container');
   if (isMobile) {
     document.getElementById('user-date').after(socialContainer);
-    iconsContainer.addEventListener('click', shareContentMobile);
+    shareButton.addEventListener('click', shareContentMobile);
   } else {
-    iconsContainer.removeEventListener('click', shareContentMobile);
-    iconsContainer.addEventListener('click', shareContentDesktop);
-    iconsContainer.addEventListener('blur', closeShareBox);
-    iconsContainer.appendChild(document.getElementById('social-icons'));
+    shareButton.removeEventListener('click', shareContentMobile);
+    shareButton.addEventListener('click', shareContentDesktop);
+    shareButton.addEventListener('blur', closeShareBox);
+    shareButton.appendChild(document.getElementById('social-container'));
   }
 }
 
